ab60cbefac74be8ed032ad4b8174234013565ec3
LoaiRadwan <loairadwan6@gmail.com>
2025-08-04 20:31:15 +0100

    check contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Container from "../Container";
 import Title from "../Title";
 import Input from "./Input";
@@ -6,7 +6,8 @@ import Input from "./Input";
 
 export default function Contact() {
 
-    const [submitState ,setSubmitState]  = useState(false)
+    // const [submitState ,setSubmitState]  = useState(false)
+    const nameInput = useRef()
 
     // function handleClick(){
     //     setSubmitState(true)
@@ -26,12 +27,12 @@ export default function Contact() {
             {/* Disable Captcha  */}
             <input type="hidden" name="_captcha" value='false'/>
 
-            <input type="hidden" name="_next" value='https://loai-radwan.github.io/profile/thank.html'/>
+            <input type="hidden" name="_next" value='https://loai-radwan.github.io/profile/'/>
 
-            <input type="hidden" name="_subject" value="New submission!" />
+            <input type="hidden" name="_subject" value={`New submission! from ${nameInput.current.value}`} />
 
 
-                <Input placeholder='Enter Your Name' type='text' name='name' />
+                <Input ref={nameInput} placeholder='Enter Your Name' type='text' name='name' />
                 <Input placeholder='Enter Your Email' type='email' name='email' />
                 <Input placeholder='Enter Your Message' isTextarea={true} name='message' />
                 <Input  value='Send Message' type='Submit' classes=' hover:bg-[var(--border-color)] duration-300 font-bold '/>
