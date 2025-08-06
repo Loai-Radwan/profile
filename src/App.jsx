68b97b38eb9df6f09785a7ec6ba78cd2db2deaeb
LoaiRadwan <loairadwan6@gmail.com>
2025-08-06 18:20:55 +0100

    enhance the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import './App.css'
-// import images from './assets/imgs/page-log.png'
 import Header from './components/Header'
-import Container from './components/Container'
 import Footer from './components/Footer'
 import Contact from './components/Contact/Contact'
 import Home from './components/Home/Home'
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import Container from './Container'
 
 export default function Header({onSelect,currentPage,logo , l}) {
+    console.log(currentPage)
 
     return (
         <header className="font-inter bg-[var(--secondary-bg-color)] h-16 w-full sticky left-0 top-0 z-1000 ">
@@ -9,9 +10,9 @@ export default function Header({onSelect,currentPage,logo , l}) {
                     <img className='h-8' src={logo} alt="" />
                 </div>
                 <ul className="links flex gap-4 ">
-                    <li onClick={onSelect} data-name='Home' className={`text-[var(--secondary-color)] duration-300 cursor-pointer hover:text-[var(--text-color)]  ${currentPage === 'Home' ? 'text-[var(--text-color)]' :''}`}  >{l.nav.home}</li>
-                    <li onClick={onSelect} data-name='Projects' className={`text-[var(--secondary-color)] duration-300 cursor-pointer hover:text-[var(--text-color)] ${currentPage === 'Projects' ? 'text-[var(--text-color)]' :''}`}  >{l.nav.projects}</li>
-                    <li onClick={onSelect} data-name='Contact' className={`text-[var(--secondary-color)] duration-300 cursor-pointer hover:text-[var(--text-color)] ${currentPage === 'Contact' ? 'text-[var(--text-color)]' :''}`}  >{l.nav.contact}</li>
+                    <li onClick={onSelect} data-name='Home' className={` duration-300 cursor-pointer hover:text-[var(--border-color)]  ${currentPage === 'Home' ? ' text-[var(--border-color)]' :''}`}  >{l.nav.home}</li>
+                    <li onClick={onSelect} data-name='Projects' className={` duration-300 cursor-pointer hover:text-[var(--border-color)] ${currentPage === 'Projects' ? ' text-[var(--border-color)]' :''}`}  >{l.nav.projects}</li>
+                    <li onClick={onSelect} data-name='Contact' className={` duration-300 cursor-pointer hover:text-[var(--border-color)] ${currentPage === 'Contact' ? ' text-[var(--border-color)]' :''}`}  >{l.nav.contact}</li>
                 </ul>
             </Container>
 
