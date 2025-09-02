eaa5f5d1d52408ff891ab2ba9d1f4833cd056743
LoaiRadwan <loairadwan6@gmail.com>
2025-09-02 06:11:18 +0100

    edit animation in arbic

--- src/components/Home/Skill.jsx
+++ src/components/Home/Skill.jsx
@@ -4,9 +4,10 @@ import Button from "../Button";
 export default function Skill({name , image ,l}){
 
     const learning = <span className="text-xs text-[var(--text-color)]/80 mx-2" >{l.skills.currentlyLearning}</span>
+        let fade = document.children[0].getAttribute('dir') == 'ltr' ? 'fadeInLeft' :'fadeInRight'
 
     return (
-        <Button classes='grid-col-1 flex flex-col item-center hover:scale-105 duration-300 my-4 fadeInLeft'>
+        <Button classes={`grid-col-1 flex flex-col item-center hover:scale-105 duration-300 my-4  ${fade}`}>
             <div className="grow basis-[150px] flex items-center justify-center m-2" >
                 <img src={image} loading="lazy" alt={`${name} logo`} /></div>
             
