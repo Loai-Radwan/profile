9918cce6ddf4f3ecdddccb54fbd373b156a04931
LoaiRadwan <loairadwan6@gmail.com>
2026-06-12 12:33:18 +0100

    update contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer({logo, l}) {
                 <img className='h-8' src={logo} alt="logo" />
                 <p className='w-full md:w-[50%] text-[var(--secondary-color)] mt-2' >{l.footer.quote}</p>
 
-                <p className='mt-2' > <i className="fa-solid fa-envelope mr-3"></i> loairalshujaa@gmail.com </p>
+                <p className='mt-2' > <i className="fa-solid fa-envelope mr-3"></i> loaialshujaa3@gmail.com </p>
                 <p className='mt-2' ><i className="fa-solid fa-phone-volume mr-3"></i>  +447778792220</p>
             </div>
             <div>
@@ -28,7 +28,7 @@ export default function Footer({logo, l}) {
                     <Social link="https://github.com/Loai-Radwan"  aria='visit me GitHub '
                     icon={<i className="fa-brands fa-github"></i>} ></Social>
 
-                    <Social link="https://www.linkedin.com/in/loai-alshjaa" aria='visit me Linkedin account' icon={ <i className="fa-brands fa-linkedin-in"></i>} ></Social>
+                    <Social link="https://www.linkedin.com/in/loai-alshujaa" aria='visit me Linkedin account' icon={ <i className="fa-brands fa-linkedin-in"></i>} ></Social>
 
                     <Social link="mailto:loaialshujaa3@gmail.com" aria='email' icon={<i className="fa-solid fa-envelope"></i>}></Social>
 
